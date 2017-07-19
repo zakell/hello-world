ec13ba86525fd8777e2d54d0be62c8bfc262febb
zakell <zakaria.ellougbi@gmail.com>
2017-07-19 16:38:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP+Docker Test</title>
  </head>
  <body>
- <?php echo '<p>DO YOU SEEE MEEEEEEEE XD, this is a test & an active change test !</p>'; ?> 
+ <?php echo '<p>XDXDDXDXDXDXDXDDDDDD XD, this is a test & an active change test !</p>'; ?> 
  </body>
 </html>
